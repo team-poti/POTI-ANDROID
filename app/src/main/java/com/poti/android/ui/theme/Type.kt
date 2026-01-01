3af5c7859bb7ba6b00d0a30cb877be74d7c09ed1
Jyunee54 <jyunee54@gmail.com>
2026-01-01 23:13:14 +0900

    [init/#1] 폰트 패딩 제거

--- app/src/main/java/com/poti/android/ui/theme/Type.kt
+++ app/src/main/java/com/poti/android/ui/theme/Type.kt
@@ -1,14 +1,26 @@
 package com.poti.android.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
+
+val PotiBaseTextStyle = TextStyle(
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None,
+    ),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
+    bodyLarge = PotiBaseTextStyle.copy(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
@@ -16,14 +28,14 @@ val Typography = Typography(
         letterSpacing = 0.5.sp,
     ),
     /* Other default text styles to override
-    titleLarge = TextStyle(
+    titleLarge = PotiBaseTextStyle.copy(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
+    labelSmall = PotiBaseTextStyle.copy(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
