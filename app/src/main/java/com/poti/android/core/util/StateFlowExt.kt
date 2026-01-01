d3e6aa115054773d622ef46cfef6148e4c478f40
Jyunee54 <jyunee54@gmail.com>
2026-01-02 02:48:46 +0900

    [init/#1] StateFlow 확장 함수

---
+++ app/src/main/java/com/poti/android/core/util/StateFlowExt.kt
@@ -0,0 +1,15 @@
+package com.poti.android.core.util
+
+import com.poti.android.core.base.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
+inline fun <T> MutableStateFlow<UiState<T>>.updateSuccess(crossinline onUpdate: (T) -> T) {
+    update { currentState ->
+        if (currentState is UiState.Success) {
+            currentState.copy(data = onUpdate(currentState.data))
+        } else {
+            currentState
+        }
+    }
+}
