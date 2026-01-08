7e3dadf614b8341ca80baf81abc138830f4b4751
Yerim Son <sonyerim12@gmail.com>
2026-01-08 18:46:38 +0900

    [Refactor/#5] lineHeightStyle 적용

--- app/src/main/java/com/poti/android/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/poti/android/core/designsystem/theme/Type.kt
@@ -2,13 +2,25 @@ package com.poti.android.core.designsystem.theme
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.poti.android.R
+
+val PotiBaseTextStyle = TextStyle(
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None,
+    ),
+)
 
 val PotiFontBold = FontFamily(Font(R.font.pretendard_bold))
 val PotiFontSemiBold = FontFamily(Font(R.font.pretendard_semibold))
@@ -30,77 +42,77 @@ data class PotiTypography(
 )
 
 val defaultPotiTypography = PotiTypography(
-    display20b = TextStyle(
+    display20b = PotiBaseTextStyle.copy(
         fontFamily = PotiFontBold,
         fontWeight = FontWeight.W700,
         fontSize = 20.sp,
         lineHeight = 1.4.em,
         letterSpacing = 0.sp,
     ),
-    display18b = TextStyle(
+    display18b = PotiBaseTextStyle.copy(
         fontFamily = PotiFontBold,
         fontWeight = FontWeight.W700,
         fontSize = 18.sp,
         lineHeight = 1.4.em,
         letterSpacing = 0.sp,
     ),
-    title18sb = TextStyle(
+    title18sb = PotiBaseTextStyle.copy(
         fontFamily = PotiFontSemiBold,
         fontWeight = FontWeight.W600,
         fontSize = 18.sp,
         lineHeight = 1.4.em,
         letterSpacing = 0.sp,
     ),
-    body16sb = TextStyle(
+    body16sb = PotiBaseTextStyle.copy(
         fontFamily = PotiFontSemiBold,
         fontWeight = FontWeight.W600,
         fontSize = 16.sp,
         lineHeight = 1.5.em,
         letterSpacing = 0.sp,
     ),
-    body16m = TextStyle(
+    body16m = PotiBaseTextStyle.copy(
         fontFamily = PotiFontMedium,
         fontWeight = FontWeight.W500,
         fontSize = 16.sp,
         lineHeight = 1.5.em,
         letterSpacing = 0.sp,
     ),
-    body14sb = TextStyle(
+    body14sb = PotiBaseTextStyle.copy(
         fontFamily = PotiFontSemiBold,
         fontWeight = FontWeight.W600,
         fontSize = 14.sp,
         lineHeight = 1.5.em,
         letterSpacing = 0.sp,
     ),
-    body14m = TextStyle(
+    body14m = PotiBaseTextStyle.copy(
         fontFamily = PotiFontMedium,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp,
         lineHeight = 1.5.em,
         letterSpacing = 0.sp,
     ),
-    caption12m = TextStyle(
+    caption12m = PotiBaseTextStyle.copy(
         fontFamily = PotiFontMedium,
         fontWeight = FontWeight.W500,
         fontSize = 12.sp,
         lineHeight = 1.5.em,
         letterSpacing = 0.sp,
     ),
-    caption10m = TextStyle(
+    caption10m = PotiBaseTextStyle.copy(
         fontFamily = PotiFontMedium,
         fontWeight = FontWeight.W500,
         fontSize = 10.sp,
         lineHeight = 1.5.em,
         letterSpacing = 0.sp,
     ),
-    button16sb = TextStyle(
+    button16sb = PotiBaseTextStyle.copy(
         fontFamily = PotiFontSemiBold,
         fontWeight = FontWeight.W600,
         fontSize = 16.sp,
         lineHeight = 1.5.em,
         letterSpacing = 0.sp,
     ),
-    button14sb = TextStyle(
+    button14sb = PotiBaseTextStyle.copy(
         fontFamily = PotiFontSemiBold,
         fontWeight = FontWeight.W600,
         fontSize = 14.sp,
