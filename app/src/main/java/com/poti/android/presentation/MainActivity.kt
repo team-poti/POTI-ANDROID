0125aea66f39a2d16e7df0e95691d38313d2be6f
Jyunee54 <jyunee54@gmail.com>
2026-01-08 13:43:24 +0900

    [Init/#4] 스플래시 구현

--- app/src/main/java/com/poti/android/presentation/MainActivity.kt
+++ app/src/main/java/com/poti/android/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.poti.android.core.designsystem.theme.PotiTheme
 import com.poti.android.presentation.main.MainScreen
 import com.poti.android.presentation.navigation.PotiNavigator
@@ -11,6 +12,7 @@ import com.poti.android.presentation.navigation.rememberPotiNavigator
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
