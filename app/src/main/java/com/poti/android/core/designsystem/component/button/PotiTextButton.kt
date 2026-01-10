682dd224a3a123cc49c5e8dccd05738568699348
Yerim Son <sonyerim12@gmail.com>
2026-01-10 20:50:36 +0900

    [Feat/#10] PotiTextButton 구현

---
+++ app/src/main/java/com/poti/android/core/designsystem/component/button/PotiTextButton.kt
@@ -0,0 +1,52 @@
+package com.poti.android.core.designsystem.component.button
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.poti.android.core.common.extension.noRippleClickable
+import com.poti.android.core.designsystem.theme.PotiTheme
+
+@Composable
+fun PotiTextButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .height(48.dp)
+            .noRippleClickable { onClick() }
+            .padding(10.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = text,
+            color = PotiTheme.colors.gray800,
+            style = PotiTheme.typography.body14m,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PotiTextButtonPreview() {
+    PotiTheme {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            PotiTextButton(
+                text = "더보기",
+                onClick = {},
+            )
+        }
+    }
+}
