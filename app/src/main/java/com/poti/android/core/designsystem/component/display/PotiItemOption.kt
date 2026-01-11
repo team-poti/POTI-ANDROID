fa86b8bd106f0a9d45ea0bde6578623fb71107e8
CheonMinJae <cmjcmj1011@gmail.com>
2026-01-12 03:05:25 +0900

    [feat/#13] item-option implement

---
+++ app/src/main/java/com/poti/android/core/designsystem/component/display/PotiItemOption.kt
@@ -0,0 +1,74 @@
+package com.poti.android.core.designsystem.component.display
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.poti.android.R
+import com.poti.android.core.designsystem.theme.PotiTheme
+import com.poti.android.core.designsystem.theme.PotiTheme.colors
+import com.poti.android.core.designsystem.theme.PotiTheme.typography
+
+enum class PotiItemOptionType(val iconResId: Int) {
+    MEMBER(R.drawable.ic_member),
+    DELIVERY(R.drawable.ic_delivery),
+    PRICE(R.drawable.ic_price);
+}
+
+enum class PotiItemOptionSize(val textStyle: @Composable () -> TextStyle) {
+    LG(textStyle = { typography.body16m }),
+    SM(textStyle = { typography.body14m });
+}
+
+@Composable
+fun PotiItemOption(
+    type: PotiItemOptionType,
+    size: PotiItemOptionSize,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = type.iconResId),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = colors.gray800
+        )
+        Text(
+            text = text,
+            style = size.textStyle(),
+            color = colors.gray800
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PotiItemOptionPreview() {
+    PotiTheme {
+        Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+            PotiItemOption(type = PotiItemOptionType.MEMBER, size = PotiItemOptionSize.LG, text = "멤버명")
+            PotiItemOption(type = PotiItemOptionType.DELIVERY, size = PotiItemOptionSize.LG, text = "배송방법")
+            PotiItemOption(type = PotiItemOptionType.PRICE, size = PotiItemOptionSize.LG, text = "9,000원")
+
+            PotiItemOption(type = PotiItemOptionType.MEMBER, size = PotiItemOptionSize.SM, text = "멤버명")
+            PotiItemOption(type = PotiItemOptionType.DELIVERY, size = PotiItemOptionSize.SM, text = "배송방법")
+            PotiItemOption(type = PotiItemOptionType.PRICE, size = PotiItemOptionSize.SM, text = "9,000원")
+        }
+    }
+}
