42ff9f926459efa05fb69e987e5032d4029bd98e
Yerim Son <sonyerim12@gmail.com>
2026-01-11 16:28:03 +0900

    [Chore/#10] klint 적용

--- app/src/main/java/com/poti/android/core/designsystem/component/button/PotiModalButton.kt
+++ app/src/main/java/com/poti/android/core/designsystem/component/button/PotiModalButton.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
