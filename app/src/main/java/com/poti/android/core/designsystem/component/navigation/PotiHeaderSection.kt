e417218dca7e80fa3ca3b7cd35474a42f2f22b75
Jyunee54 <jyunee54@gmail.com>
2026-01-14 01:50:32 +0900

    [Chore/#29] 색상 수정

--- app/src/main/java/com/poti/android/core/designsystem/component/navigation/PotiHeaderSection.kt
+++ app/src/main/java/com/poti/android/core/designsystem/component/navigation/PotiHeaderSection.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,7 +47,7 @@ fun PotiHeaderSection(
         modifier = modifier,
         divider = {
             HorizontalDivider(
-                color = Color(0xFFDCDCDC), // TODO: [지현] 컬러 시스템 변경
+                color = PotiTheme.colors.gray300,
                 thickness = 2.dp,
             )
         },
