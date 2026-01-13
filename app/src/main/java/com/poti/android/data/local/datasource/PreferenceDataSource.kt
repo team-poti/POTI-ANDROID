c68081d15366a0b9b18cf1759c8270d922d68f7b
Jyunee54 <jyunee54@gmail.com>
2026-01-14 04:38:09 +0900

    [feat/#29] DataStore 토큰 저장

--- app/src/main/java/com/poti/android/data/local/datasource/PreferenceDataSource.kt
+++ app/src/main/java/com/poti/android/data/local/datasource/PreferenceDataSource.kt
@@ -1,9 +1,48 @@
 package com.poti.android.data.local.datasource
 
 import androidx.datastore.core.DataStore
-import java.util.prefs.Preferences
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class PreferenceDataSource(
+class PreferenceDataSource @Inject constructor(
     private val dataStore: DataStore<Preferences>,
 ) {
+    companion object {
+        private val ACCESS_TOKEN_KEY = stringPreferencesKey("ACCESS_TOKEN")
+        private val REFRESH_TOKEN_KEY = stringPreferencesKey("REFRESH_TOKEN")
+    }
+
+    suspend fun saveAccessToken(accessToken: String) = dataStore.edit { prefs ->
+        prefs[ACCESS_TOKEN_KEY] = accessToken
+    }
+
+    suspend fun saveRefreshToken(refreshToken: String) = dataStore.edit { prefs ->
+        prefs[REFRESH_TOKEN_KEY] = refreshToken
+    }
+
+    suspend fun saveTokens(
+        accessToken: String,
+        refreshToken: String,
+    ) = dataStore.edit { prefs ->
+        prefs[ACCESS_TOKEN_KEY] = accessToken
+        prefs[REFRESH_TOKEN_KEY] = refreshToken
+    }
+
+    val accessToken: Flow<String?> = dataStore.data.map { prefs ->
+        prefs[ACCESS_TOKEN_KEY]
+    }
+
+    val refreshToken: Flow<String?> = dataStore.data.map { prefs ->
+        prefs[REFRESH_TOKEN_KEY]
+    }
+
+    suspend fun clear() {
+        dataStore.edit { prefs ->
+            prefs.clear()
+        }
+    }
 }
--- app/src/main/java/com/poti/android/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/poti/android/data/repository/AuthRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.poti.android.data.repository
 
 import com.poti.android.core.network.model.handleApiResponse
 import com.poti.android.core.network.util.HttpResponseHandler
+import com.poti.android.data.local.datasource.PreferenceDataSource
 import com.poti.android.data.mapper.toDomain
 import com.poti.android.data.remote.datasource.AuthRemoteDataSource
 import com.poti.android.domain.model.UserAuth
@@ -11,6 +12,7 @@ import javax.inject.Inject
 class AuthRepositoryImpl @Inject constructor(
     private val httpResponseHandler: HttpResponseHandler,
     private val authRemoteDataSource: AuthRemoteDataSource,
+    private val preferenceDataSource: PreferenceDataSource,
 ) : AuthRepository {
     override suspend fun login(
         socialType: String,
@@ -19,6 +21,7 @@ class AuthRepositoryImpl @Inject constructor(
         authRemoteDataSource.login(socialType = socialType, token = token)
             .handleApiResponse()
             .getOrThrow()
+            .apply { preferenceDataSource.saveTokens(accessToken, refreshToken) }
             .toDomain()
     }
 }
