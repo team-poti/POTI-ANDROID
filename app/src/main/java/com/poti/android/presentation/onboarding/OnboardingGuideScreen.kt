a0d85d191b1cdc775c07b0eb2ee92f78a0ef6405
Jyunee54 <jyunee54@gmail.com>
2026-01-14 22:32:02 +0900

    [feat/#33] Onboarding 컴포넌트

--- app/src/main/java/com/poti/android/presentation/onboarding/OnboardingGuideScreen.kt
+++ app/src/main/java/com/poti/android/presentation/onboarding/OnboardingGuideScreen.kt
@@ -1,13 +1,46 @@
 package com.poti.android.presentation.onboarding
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.poti.android.R
+import com.poti.android.core.common.util.screenWidthDp
+import com.poti.android.core.designsystem.component.display.PotiStepper
+import com.poti.android.core.designsystem.component.navigation.PotiHeaderPage
+import com.poti.android.core.designsystem.theme.PotiTheme
 
 @Composable
 fun OnboardingGuideRoute(modifier: Modifier = Modifier) {
-    OnboardingGuideScreen(modifier = modifier)
+    OnboardingGuideScreen(
+        onNavigateToLogin = {},
+        modifier = modifier,
+    )
 }
 
 @Composable
-private fun OnboardingGuideScreen(modifier: Modifier = Modifier) {
+private fun OnboardingGuideScreen(
+    onNavigateToLogin: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        PotiHeaderPage(onNavigationClick = onNavigateToLogin)
+
+        PotiStepper(
+            step = 1,
+            modifier = Modifier.padding(vertical = 4.dp),
+        )
+
+        Text(
+            text = stringResource(R.string.onboarding_Guide_label),
+            style = PotiTheme.typography.title18sb,
+            color = PotiTheme.colors.black,
+            modifier = Modifier.padding(horizontal = screenWidthDp(20.dp), vertical = 24.dp),
+        )
+    }
 }
