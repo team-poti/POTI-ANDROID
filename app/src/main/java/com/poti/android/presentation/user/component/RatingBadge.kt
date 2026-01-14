c34e3daaba2006f91d121f89b8fa88b7553436f2
Yerim Son <sonyerim12@gmail.com>
2026-01-14 20:31:43 +0900

    [Refactor/#23] RatingBadge 클릭 제거 및 최소 너비 지정

--- app/src/main/java/com/poti/android/presentation/user/component/RatingBadge.kt
+++ app/src/main/java/com/poti/android/presentation/user/component/RatingBadge.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,22 +15,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.poti.android.core.common.extension.noRippleClickable
 import com.poti.android.core.designsystem.component.display.PotiRating
 import com.poti.android.core.designsystem.theme.PotiTheme
 
 @Composable
 fun RatingBadge(
     rating: String,
-    onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Row(
         modifier = modifier
             .heightIn(min = 40.dp)
+            .widthIn(min = 71.dp)
             .clip(CircleShape)
             .background(PotiTheme.colors.black)
-            .noRippleClickable(onClick)
             .padding(horizontal = 12.dp, vertical = 8.dp),
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically,
@@ -52,7 +51,6 @@ private fun RatingBadgePreview() {
         ) {
             RatingBadge(
                 rating = "4.8",
-                onClick = {},
                 modifier = Modifier.width(71.dp),
             )
         }
--- app/src/main/java/com/poti/android/presentation/user/profile/ProfileScreen.kt
+++ app/src/main/java/com/poti/android/presentation/user/profile/ProfileScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
@@ -85,15 +84,12 @@ private fun ProfileScreen(
                 imageUrl = imageUrl,
                 nickname = nickname,
                 email = email,
-                modifier = Modifier,
             )
 
             Spacer(Modifier.height(24.dp))
 
             RatingBadge(
                 rating = rating,
-                onClick = {},
-                modifier = Modifier.width(71.dp),
             )
 
             Spacer(Modifier.height(24.dp))
