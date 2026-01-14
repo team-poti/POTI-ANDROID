0661333584ea2b27e553e64b8d6b15704408d510
Jyunee54 <jyunee54@gmail.com>
2026-01-15 04:03:21 +0900

    [feat/#35] 분철팟 상세 뷰 UI 구현

--- app/src/main/java/com/poti/android/presentation/party/detail/PartyDetailViewModel.kt
+++ app/src/main/java/com/poti/android/presentation/party/detail/PartyDetailViewModel.kt
@@ -1,7 +1,7 @@
 package com.poti.android.presentation.party.detail
 
 import androidx.lifecycle.ViewModel
-import com.poti.android.presentation.party.detail.model.Contracts
+import com.poti.android.presentation.party.detail.model.PartyDetailUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,6 +9,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PartyDetailViewModel @Inject constructor() : ViewModel() {
-    private val _uiState = MutableStateFlow(Contracts())
+    private val _uiState = MutableStateFlow(PartyDetailUiState())
     val uiState = _uiState.asStateFlow()
 }
