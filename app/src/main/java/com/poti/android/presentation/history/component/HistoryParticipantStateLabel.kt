ebd6eaec7d690f9022b9e84940b9e80dab3a60d6
CheonMinJae <cmjcmj1011@gmail.com>
2026-01-14 21:47:44 +0900

    [chore/#25] apply ktlint

--- app/src/main/java/com/poti/android/presentation/history/component/HistoryParticipantStateLabel.kt
+++ app/src/main/java/com/poti/android/presentation/history/component/HistoryParticipantStateLabel.kt
@@ -2,11 +2,8 @@ package com.poti.android.presentation.history.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
