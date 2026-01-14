88db02f54760b45904a9f9343c0eaa0b76b07077
CheonMinJae <cmjcmj1011@gmail.com>
2026-01-14 16:15:33 +0900

    [feat/#25] apply ktlint

--- app/src/main/java/com/poti/android/presentation/history/component/HistoryStateGuide.kt
+++ app/src/main/java/com/poti/android/presentation/history/component/HistoryStateGuide.kt
@@ -1,13 +1,11 @@
 package com.poti.android.presentation.history.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
