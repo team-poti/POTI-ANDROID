0eb41d00fbd895ed24131c354c31bc334e54155f
dodo <eg4rang@naver.com>
2026-01-15 17:47:46 +0900

    [Feat/#31] 갤러리 이미지 선택 기능 추가

---
+++ app/src/main/java/com/poti/android/presentation/party/create/component/CreatePhotoUpload.kt
@@ -0,0 +1,165 @@
+package com.poti.android.presentation.party.create.component
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.poti.android.R
+import com.poti.android.core.common.util.screenWidthDp
+import com.poti.android.core.designsystem.component.button.DeleteButtonType
+import com.poti.android.core.designsystem.component.button.PotiDeleteButton
+import com.poti.android.core.designsystem.theme.PotiTheme
+
+private const val MAX_ITEMS = 5
+
+@Composable
+fun CreatePhotoUpload(
+    imageUris: List<Uri>,
+    onImageChanged: (List<Uri>) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val scrollState = rememberScrollState()
+
+    val remaining = MAX_ITEMS - imageUris.size
+
+    val photoPickerLauncher = rememberLauncherForActivityResult(
+        contract = when {
+            remaining <= 1 -> ActivityResultContracts.PickVisualMedia()
+            else -> ActivityResultContracts.PickMultipleVisualMedia(remaining)
+        },
+    ) { result ->
+        when (result) {
+            is Uri -> {
+                onImageChanged(imageUris + result)
+            }
+
+            is List<*> -> {
+                val uris = result.filterIsInstance<Uri>()
+                onImageChanged(imageUris + uris)
+            }
+        }
+    }
+
+    Row(
+        modifier = modifier
+            .padding(vertical = 8.dp)
+            .horizontalScroll(scrollState),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+        if (imageUris.size < MAX_ITEMS) {
+            UploadButton(
+                onClick = { photoPickerLauncher.launch(PickVisualMediaRequest()) },
+            )
+        }
+
+        imageUris.forEachIndexed { index, uri ->
+            PhotoItem(
+                image = uri,
+                onXClick = {
+                    val newUriList = imageUris.filterIndexed { i, _ -> i != index }
+                    onImageChanged(newUriList)
+                },
+            )
+        }
+    }
+}
+
+@Composable
+private fun UploadButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(PotiTheme.colors.gray100)
+            .clickable(
+                onClick = onClick,
+                enabled = enabled,
+            )
+            .size(screenWidthDp(90.dp)),
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_plus),
+            contentDescription = null,
+            tint = PotiTheme.colors.gray700,
+        )
+    }
+}
+
+@Composable
+private fun PhotoItem(
+    image: Uri,
+    onXClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .size(screenWidthDp(90.dp))
+            .clip(RoundedCornerShape(8.dp)),
+    ) {
+        AsyncImage(
+            model = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Gray),
+            contentScale = ContentScale.Crop,
+        )
+
+        PotiDeleteButton(
+            onClick = onXClick,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .offset(x = 8.dp, y = (-8).dp),
+            type = DeleteButtonType.LIGHT,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CreatePhotoUploadPreview() {
+    var imageUris by remember { mutableStateOf<List<Uri>>((emptyList())) }
+
+    PotiTheme {
+        CreatePhotoUpload(
+            imageUris = imageUris,
+            onImageChanged = { imageUris = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 100.dp),
+        )
+    }
+}
