082467d81366837238a6360590970da58ff4669d
Yerim Son <sonyerim12@gmail.com>
2026-01-17 02:52:00 +0900

    [Chore/#47] klint

--- app/src/main/java/com/poti/android/presentation/party/home/model/Contracts.kt
+++ app/src/main/java/com/poti/android/presentation/party/home/model/Contracts.kt
@@ -1,5 +1,7 @@
 package com.poti.android.presentation.party.home.model
 
+import com.poti.android.core.base.UiEffect
+import com.poti.android.core.base.UiIntent
 import com.poti.android.core.base.UiState
 import com.poti.android.core.common.state.ApiState
 import com.poti.android.domain.model.home.HomeContent
@@ -7,3 +9,9 @@ import com.poti.android.domain.model.home.HomeContent
 data class HomeUiState(
     val homeContentLoadState: ApiState<HomeContent> = ApiState.Loading,
 ) : UiState
+
+sealed interface HomeUiIntent : UiIntent {
+}
+
+sealed interface HomeUiEffect : UiEffect {
+}
