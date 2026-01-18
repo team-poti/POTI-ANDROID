d19d79242b2135a6c0901f64e60d4dc97ebceb8d
Yerim Son <sonyerim12@gmail.com>
2026-01-19 08:02:11 +0900

    [Refactor/#59] 코드 리뷰 반영

--- app/src/main/java/com/poti/android/presentation/party/goodsfilter/GoodsCategoryScreen.kt
+++ app/src/main/java/com/poti/android/presentation/party/goodsfilter/GoodsCategoryScreen.kt
@@ -1,16 +1,16 @@
 package com.poti.android.presentation.party.goodsfilter
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,16 +59,14 @@ private fun GoodsCategoryScreen(
                 .padding(innerPadding),
             contentPadding = PaddingValues(horizontal = screenWidthDp(16.dp)),
         ) {
-            stickyHeader {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
-                ) {
-                    PotiSmallButton(
-                        text = "인기순", // TODO: 바텀시트 연결
-                        onClick = onSortFilterClick,
-                    )
-                }
+            item {
+                PotiSmallButton(
+                    text = "인기순", // TODO: 바텀시트 연결
+                    onClick = onSortFilterClick,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.End),
+                )
             }
 
             items(goodsCategory.groupItems) { groupItem ->
