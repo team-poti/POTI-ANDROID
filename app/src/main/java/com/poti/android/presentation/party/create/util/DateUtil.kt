fc841952e41d5496b71c62677dfb28b90195b8ee
dodo <eg4rang@naver.com>
2026-01-21 03:12:09 +0900

    [Feat/#85] 날짜 변환 및 비교 유틸

---
+++ app/src/main/java/com/poti/android/presentation/party/create/util/DateUtil.kt
@@ -0,0 +1,23 @@
+package com.poti.android.presentation.party.create.util
+
+import android.util.Log
+import androidx.core.text.isDigitsOnly
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+
+fun String.toDateOrNull(): LocalDate? {
+    return try {
+        if (!this.isDigitsOnly() || this.length != 8) return null
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        return LocalDate.parse(this, formatter)
+    } catch (_: DateTimeParseException) {
+        Log.d("아아", "실패임")
+        null
+    }
+}
+
+fun LocalDate.isTodayOrAfter(): Boolean {
+    val today = LocalDate.now()
+    return this.isAfter(today) || this.isEqual(today)
+}
