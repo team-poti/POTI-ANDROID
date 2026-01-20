b651931bcdc92ad555b874ea3f283c5d34a99149
dodo <eg4rang@naver.com>
2026-01-20 23:54:14 +0900

    [Feat/#72] 등록 네비게이션

--- app/src/main/java/com/poti/android/presentation/party/create/navigation/PartyCreateNavigation.kt
+++ app/src/main/java/com/poti/android/presentation/party/create/navigation/PartyCreateNavigation.kt
@@ -1,10 +1,16 @@
 package com.poti.android.presentation.party.create.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.poti.android.core.common.extension.sharedViewModel
 import com.poti.android.core.navigation.Route
+import com.poti.android.presentation.party.create.PartyArtistSelectRoute
+import com.poti.android.presentation.party.create.PartyCreateRoute
+import com.poti.android.presentation.party.create.PartyCreateViewModel
 import kotlinx.serialization.Serializable
 
 sealed interface PartyCreateRoute : Route {
@@ -24,8 +30,24 @@ fun NavController.navigateToPartyArtistSelect() {
 }
 
 fun NavGraphBuilder.partyCreateNavGraph(
+    navController: NavController,
     paddingValues: PaddingValues,
 ) {
-    composable<PartyCreateRoute.Create> { }
-    composable<PartyCreateRoute.ArtistSelect> { }
+    composable<PartyCreateRoute.Create> {entry ->
+        val viewModel : PartyCreateViewModel = entry.sharedViewModel(navController)
+        PartyCreateRoute(
+            onPopBackStack = navController::popBackStack,
+            onNavigateToSearch = navController::navigateToPartyArtistSelect,
+            viewModel = viewModel,
+            modifier = Modifier.padding(paddingValues),
+        )
+    }
+    composable<PartyCreateRoute.ArtistSelect> {entry ->
+        val viewModel : PartyCreateViewModel = entry.sharedViewModel(navController)
+        PartyArtistSelectRoute(
+            onPopBackStack = navController::popBackStack,
+            viewModel = viewModel,
+            modifier = Modifier.padding(paddingValues),
+        )
+    }
 }
