2a5ec246978b7448329ca03edf13c2e56de3fbbb
Jyunee54 <jyunee54@gmail.com>
2026-01-21 00:06:39 +0900

    [feat/#48] 키보드 타입 수정

--- app/src/main/java/com/poti/android/presentation/party/detail/PartyJoinScreen.kt
+++ app/src/main/java/com/poti/android/presentation/party/detail/PartyJoinScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -173,6 +174,7 @@ private fun PartyJoinScreen(
                         placeholder = stringResource(R.string.party_join_order_postal_placeholder),
                         label = stringResource(R.string.party_join_order_postal_label),
                         error = if (uiState.isPostalCodeError) stringResource(R.string.party_join_order_postal_error) else "",
+                        keyboardType = KeyboardType.Number,
                     )
 
                     PotiShortTextField(
@@ -189,6 +191,7 @@ private fun PartyJoinScreen(
                         placeholder = stringResource(R.string.party_join_order_contact_placeholder),
                         label = stringResource(R.string.party_join_order_contact_label),
                         error = if (uiState.isContactError) stringResource(R.string.party_join_order_contact_error) else "",
+                        keyboardType = KeyboardType.Number,
                     )
                 }
             }
