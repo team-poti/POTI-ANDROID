6be1e69655de02c10e6976d6d4a9705e3cc6dfa0
dodo <eg4rang@naver.com>
2026-01-20 22:11:23 +0900

    [Feat/#72] 아티스트 검색 뷰 구현

--- app/src/main/java/com/poti/android/presentation/party/create/PartyArtistSelectScreen.kt
+++ app/src/main/java/com/poti/android/presentation/party/create/PartyArtistSelectScreen.kt
@@ -1,13 +1,124 @@
 package com.poti.android.presentation.party.create
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.poti.android.R
+import com.poti.android.core.common.extension.getSuccessDataOrNull
+import com.poti.android.core.common.util.HandleSideEffects
+import com.poti.android.core.common.util.screenWidthDp
+import com.poti.android.core.designsystem.component.button.ActionButtonType
+import com.poti.android.core.designsystem.component.button.PotiActionButton
+import com.poti.android.core.designsystem.component.display.PotiEmptyStateInline
+import com.poti.android.core.designsystem.component.navigation.PotiHeaderPage
+import com.poti.android.core.designsystem.theme.PotiTheme
+import com.poti.android.domain.model.artist.Artist
+import com.poti.android.presentation.party.create.component.CreateDropdownField
+import com.poti.android.presentation.party.create.model.CreateUiEffect
+import com.poti.android.presentation.party.create.model.CreateUiIntent
+import com.poti.android.presentation.party.create.model.CreateUiState
 
 @Composable
-fun PartyArtistSelectRoute(modifier: Modifier = Modifier) {
-    PartyArtistSelectScreen(modifier = modifier)
+fun PartyArtistSelectRoute(
+    onPopBackStack: () -> Unit,
+    viewModel: PartyCreateViewModel,
+    modifier: Modifier = Modifier,
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    HandleSideEffects(viewModel.sideEffect) { effect ->
+        when (effect) {
+            CreateUiEffect.NavigateToBack -> onPopBackStack()
+            else -> Unit
+        }
+    }
+
+    PartyArtistSelectScreen(
+        uiState = uiState,
+        onSearchKeywordChange = { viewModel.processIntent(CreateUiIntent.OnArtistSearchKeywordChange(it)) },
+        onArtistSelect = { viewModel.processIntent(CreateUiIntent.OnArtistSelect(it)) },
+        onConfirmClick = {viewModel.processIntent(CreateUiIntent.OnBackClick)},
+        onPopBackStack = {viewModel.processIntent(CreateUiIntent.OnBackClick)},
+        modifier = modifier,
+    )
 }
 
 @Composable
-private fun PartyArtistSelectScreen(modifier: Modifier = Modifier) {
+private fun PartyArtistSelectScreen(
+    uiState: CreateUiState,
+    onSearchKeywordChange: (String) -> Unit,
+    onArtistSelect: (Artist) -> Unit,
+    onConfirmClick: () -> Unit,
+    onPopBackStack: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            PotiHeaderPage(
+                onNavigationClick = onPopBackStack,
+                title = stringResource(R.string.create_header_artist_search),
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            CreateDropdownField(
+                value = uiState.artistSearchKeyword,
+                onValueChanged = onSearchKeywordChange,
+                searchResults = uiState.artistSearchResultsState.getSuccessDataOrNull() ?: emptyList(),
+                resultToString = { it.name },
+                onItemClick = { onArtistSelect(it) },
+                placeholder = stringResource(R.string.create_placeholder_artist_search),
+                modifier = Modifier.padding(vertical = 12.dp),
+            )
+
+            if (uiState.isArtistSearchResultsEmpty) {
+                PotiEmptyStateInline(
+                    text = stringResource(R.string.create_message_artist_search_empty_result),
+                    modifier = Modifier.padding(horizontal = screenWidthDp(16.dp)),
+                )
+            }
+
+            Spacer(Modifier.weight(1f))
+
+            PotiActionButton(
+                text = stringResource(R.string.action_button_done),
+                onClick = onConfirmClick,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = screenWidthDp(16.dp))
+                    .padding(top = 4.dp, bottom = 16.dp),
+                type = if (uiState.isArtistSelectDoneBtnEnabled) ActionButtonType.SECONDARY_MAIN else ActionButtonType.DEACTIVE_MAIN,
+                enabled = uiState.isArtistSelectDoneBtnEnabled
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PartyArtistSelectScreenPreview() {
+    PotiTheme {
+        PartyArtistSelectScreen(
+            uiState = CreateUiState(),
+            onSearchKeywordChange = {},
+            onArtistSelect = {},
+            onConfirmClick = {},
+            onPopBackStack = {},
+        )
+    }
 }
