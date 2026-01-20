6048b3873d83bf441c42bf5c830e21d0dbbd385c
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2026-01-20 17:53:48 +0900

    [Feat/#56] 분철 내역 뷰 UI 구현 (#58)
    * [Feat/#56] HistoryListScreen UI
    
    * [Feat/#56] HistoryListViewModel
    
    * [Feat/#56] 내역 없을 때 Empty State UI 추가
    
    * [Fix/#56] PotiEmptyStateBlock을 PotiEmptyStateInline으로 변경
    
    * [Refactor/#56] 코드 리뷰 반영
    
    * [Refactor/#56] 코드 리뷰 반영
    
    * [Refactor/#57] processIntent 함수 분리
    
    * [Refactor/#57] 프레젠테이션 레이어 의존성 제거

---
+++ app/src/main/java/com/poti/android/domain/model/history/HistoryList.kt
@@ -0,0 +1,19 @@
+package com.poti.android.domain.model.history
+
+import com.poti.android.domain.type.HistoryStage
+import com.poti.android.domain.type.HistoryStatus
+
+data class HistoryListContent(
+    val ongoingCount: Int,
+    val endedCount: Int,
+    val items: List<HistoryItem>,
+)
+
+data class HistoryItem(
+    val id: Long,
+    val imageUrl: String?,
+    val artist: String,
+    val title: String,
+    val stage: HistoryStage,
+    val status: HistoryStatus,
+)
