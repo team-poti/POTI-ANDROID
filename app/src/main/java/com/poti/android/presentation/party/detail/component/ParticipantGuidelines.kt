0bb5bccdc4e4fe10f1446de19730a428a05c8301
Jyunee54 <jyunee54@gmail.com>
2026-01-21 15:43:11 +0900

    [feat/#79] 참여자 안내 사항 텍스트 추가

--- app/src/main/java/com/poti/android/presentation/party/detail/component/ParticipantGuidelines.kt
+++ app/src/main/java/com/poti/android/presentation/party/detail/component/ParticipantGuidelines.kt
@@ -1,10 +1,12 @@
 package com.poti.android.presentation.party.detail.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.unit.dp
 import com.poti.android.R
 import com.poti.android.core.common.util.screenWidthDp
@@ -14,12 +16,20 @@ import com.poti.android.core.designsystem.theme.PotiTheme
 fun ParticipantGuidelines(
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = stringResource(R.string.party_detail_announcement),
-        style = PotiTheme.typography.caption12m,
-        color = PotiTheme.colors.gray800,
+    val announcements = stringArrayResource(R.array.party_detail_announcement)
+
+    Column(
         modifier = modifier
             .padding(horizontal = screenWidthDp(16.dp), vertical = 16.dp)
             .padding(bottom = 40.dp),
-    )
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        announcements.forEach { text ->
+            Text(
+                text = text,
+                style = PotiTheme.typography.caption12m,
+                color = PotiTheme.colors.gray800,
+            )
+        }
+    }
 }
