62d9f2885e0633b09a9e30d65433f38a9e99e1d3
Yerim Son <sonyerim12@gmail.com>
2026-01-21 23:14:44 +0900

    [Feat/#93] presentation

--- app/src/main/java/com/poti/android/presentation/party/goodsfilter/GoodsCategoryViewModel.kt
+++ app/src/main/java/com/poti/android/presentation/party/goodsfilter/GoodsCategoryViewModel.kt
@@ -1,7 +1,9 @@
 package com.poti.android.presentation.party.goodsfilter
 
+import androidx.lifecycle.SavedStateHandle
 import com.poti.android.core.base.BaseViewModel
 import com.poti.android.core.common.state.ApiState
+import com.poti.android.domain.repository.HomeRepository
 import com.poti.android.presentation.party.goodsfilter.model.GoodsCategoryUiEffect
 import com.poti.android.presentation.party.goodsfilter.model.GoodsCategoryUiIntent
 import com.poti.android.presentation.party.goodsfilter.model.GoodsCategoryUiState
@@ -9,30 +11,52 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class GoodsCategoryViewModel @Inject constructor() :
+class GoodsCategoryViewModel @Inject constructor(
+    private val homeRepository: HomeRepository,
+    savedStateHandle: SavedStateHandle,
+) :
     BaseViewModel<GoodsCategoryUiState, GoodsCategoryUiIntent, GoodsCategoryUiEffect>(
-        initialState = GoodsCategoryUiState(),
-    ) {
+            initialState = GoodsCategoryUiState(),
+        ) {
+        private val artistId: Long = checkNotNull(savedStateHandle["artistId"])
+
         override fun processIntent(intent: GoodsCategoryUiIntent) {
             when (intent) {
                 GoodsCategoryUiIntent.OnBackClick -> sendEffect(GoodsCategoryUiEffect.NavigateBack)
                 GoodsCategoryUiIntent.OnFloatingClick -> sendEffect(GoodsCategoryUiEffect.NavigateToPartyCreate)
-                GoodsCategoryUiIntent.OnSortFilterClick -> {}
+                GoodsCategoryUiIntent.OnSortFilterClick -> {
+                    // TODO: [예림] 정렬 바텀시트
+                }
                 GoodsCategoryUiIntent.OnCardClick -> sendEffect(GoodsCategoryUiEffect.NavigateToGoodsFilter)
             }
         }
 
         init {
-            loadGoodsCategory()
+            loadGoodsCategoryList()
         }
 
-        private fun loadGoodsCategory() {
-            updateState {
-                copy(
-                    goodsCategoryLoadState = ApiState.Success(
-                        dummyGoodsCategory,
-                    ),
-                )
-            }
+        private fun loadGoodsCategoryList() = launchScope {
+            updateState { copy(goodsCategoryLoadState = ApiState.Loading) }
+
+            homeRepository.getGoodsCategoryList(
+                page = 0,
+                size = 10,
+                sort = "LATEST", // TODO
+                artistId = artistId,
+            )
+                .onSuccess { goodsCategory ->
+                    updateState {
+                        copy(goodsCategoryLoadState = ApiState.Success(goodsCategory))
+                    }
+                }
+                .onFailure { throwable ->
+                    updateState {
+                        copy(
+                            goodsCategoryLoadState = ApiState.Failure(
+                                throwable.message ?: "Failed to load goods category",
+                            ),
+                        )
+                    }
+                }
         }
     }
