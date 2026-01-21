55dad6ce35c58b8456f74711516117b7eca9d481
Jyunee54 <jyunee54@gmail.com>
2026-01-22 06:55:30 +0900

    [feat/#91] dto 수정

--- app/src/main/java/com/poti/android/data/remote/datasource/PartyRemoteDataSource.kt
+++ app/src/main/java/com/poti/android/data/remote/datasource/PartyRemoteDataSource.kt
@@ -4,8 +4,8 @@ import com.poti.android.core.network.model.BaseResponse
 import com.poti.android.data.remote.dto.request.party.CreatePartyRequestDto
 import com.poti.android.data.remote.dto.request.party.PartyJoinRequestDto
 import com.poti.android.data.remote.dto.response.artist.ArtistSearchListResponseDto
-import com.poti.android.data.remote.dto.response.participant.MyPartyListDto
 import com.poti.android.data.remote.dto.response.party.CreatePartyResponseDto
+import com.poti.android.data.remote.dto.response.party.MyRecruitListDto
 import com.poti.android.data.remote.dto.response.party.PartyDetailResponseDto
 import com.poti.android.data.remote.dto.response.party.PartyJoinOptionsDto
 import com.poti.android.data.remote.dto.response.party.PartyJoinResponseDto
@@ -41,6 +41,6 @@ class PartyRemoteDataSource @Inject constructor(
     suspend fun postPartyJoin(partyJoinRequest: PartyJoinRequestDto): BaseResponse<PartyJoinResponseDto> =
         partyService.postPartyJoin(partyJoinRequest = partyJoinRequest)
 
-    suspend fun getMyRecruitList(status: String): BaseResponse<MyPartyListDto> =
+    suspend fun getMyRecruitList(status: String): BaseResponse<MyRecruitListDto> =
         partyService.getMyRecruitList(status)
 }
