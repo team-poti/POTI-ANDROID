fa9cf53d08068f305547ecdae679297e93875bbd
dodo <eg4rang@naver.com>
2026-02-05 17:02:14 +0900

    [Feat/#145] 이미지 업로드 유스케이스 생성 (V2) - 다음 과정 순차 수행 : presigned-url 발급 > 리사이징된 파일 객체 생성 > s3 업로드 > 파일 객체 메모리에서 삭제

---
+++ app/src/main/java/com/poti/android/domain/usecase/image/UploadImagesUseCaseV2.kt
@@ -0,0 +1,55 @@
+package com.poti.android.domain.usecase.image
+
+import com.poti.android.core.common.constant.ImageConstants.IMAGE_EXTENSION
+import com.poti.android.domain.model.image.PresignedUploadInfo
+import com.poti.android.domain.repository.FileUploadRepository
+import com.poti.android.domain.repository.ImageRepository
+import java.io.File
+import javax.inject.Inject
+
+class UploadImagesUseCaseV2 @Inject constructor(
+    private val imageRepository: ImageRepository,
+    private val fileUploadRepository: FileUploadRepository,
+) {
+    suspend operator fun invoke(
+        uploadType: String,
+        uriStrings: List<String>,
+    ): Result<List<String>> {
+        try {
+            val uploadInfos = getUploadUrls(uploadType, uriStrings.size)
+            val (urls, fileNames) = uploadInfos.map { it.url to it.fileName }.unzip()
+            val files = createImages(uriStrings)
+
+            uploadImages(urls, files)
+            clearDirectory()
+
+            return Result.success(fileNames)
+        } catch (t: Throwable) {
+            return Result.failure(t)
+        }
+    }
+
+    private suspend fun getUploadUrls(
+        uploadType: String,
+        size: Int,
+    ): List<PresignedUploadInfo> = imageRepository.getPresignedUrls(
+        type = uploadType,
+        extensions = List(size) { IMAGE_EXTENSION },
+    ).getOrThrow()
+
+    private fun createImages(
+        uriStrings: List<String>,
+    ): List<File> = uriStrings.map { uri ->
+        fileUploadRepository.createImage(uri).getOrThrow()
+    }
+
+    private suspend fun uploadImages(
+        urls: List<String>,
+        files: List<File>,
+    ) = urls.zip(files) { url, file ->
+        fileUploadRepository.uploadImage(url, file).getOrThrow()
+    }
+
+    private fun clearDirectory() = fileUploadRepository
+        .clearDirectory().getOrThrow()
+}
