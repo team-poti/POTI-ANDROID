be47d90a5557e0fc93e7e6caa19e7dff8c14cc23
jyvnee <jyunee54@gmail.com>
2026-08-17 14:41:20 +0900

    [feat/#183] 검색 결과 DTO 추가

---
+++ app/src/main/java/com/poti/android/data/remote/dto/response/party/PartySearchResponseDto.kt
@@ -0,0 +1,28 @@
+package com.poti.android.data.remote.dto.response.party
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PartySearchResponseDto(
+    @SerialName("content")
+    val content: List<PartySearchItemResponseDto>,
+    @SerialName("hasNext")
+    val hasNext: Boolean,
+)
+
+@Serializable
+data class PartySearchItemResponseDto(
+    @SerialName("artist")
+    val artist: String,
+    @SerialName("artistId")
+    val artistId: Long,
+    @SerialName("postImage")
+    val postImage: String?,
+    @SerialName("postTitle")
+    val postTitle: String,
+    @SerialName("postCount")
+    val postCount: Long,
+    @SerialName("tag")
+    val tag: String?,
+)
