02185df6775f4a739f6ffca31f82fc144643ce66
jyvnee <jyunee54@gmail.com>
2026-08-17 14:52:10 +0900

    [feat/#183] SearchPartyUseCase 추가

---
+++ app/src/main/java/com/poti/android/domain/usecase/search/SearchPartyUseCase.kt
@@ -0,0 +1,16 @@
+package com.poti.android.domain.usecase.search
+
+import com.poti.android.domain.model.search.PartySearchResult
+import com.poti.android.domain.repository.SearchRepository
+import javax.inject.Inject
+
+class SearchPartyUseCase @Inject constructor(
+    private val searchRepository: SearchRepository,
+) {
+    suspend operator fun invoke(
+        keyword: String,
+        page: Int,
+        size: Int,
+        sort: String? = null,
+    ): Result<PartySearchResult> = searchRepository.searchParties(keyword, page, size, sort)
+}
