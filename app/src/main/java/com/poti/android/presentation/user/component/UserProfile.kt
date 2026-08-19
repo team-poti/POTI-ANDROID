3163adce99d9df43c65f790cbe6ade172a47f906
jyvnee <jyunee54@gmail.com>
2026-08-20 00:16:06 +0900

    [feat/#182] 프로필 이미지 컴포넌트 분리

--- app/src/main/java/com/poti/android/presentation/user/component/UserProfile.kt
+++ app/src/main/java/com/poti/android/presentation/user/component/UserProfile.kt
@@ -1,25 +1,16 @@
 package com.poti.android.presentation.user.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.poti.android.R
 import com.poti.android.core.designsystem.theme.PotiTheme
 
 /**
@@ -37,20 +28,7 @@ fun UserProfile(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(imageUrl)
-                .placeholder(R.drawable.img_basic_profile)
-                .error(R.drawable.img_basic_profile)
-                .crossfade(true)
-                .build(),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(98.dp)
-                .clip(CircleShape)
-                .background(PotiTheme.colors.gray100),
-        )
+        UserProfileImage(imageUrl = imageUrl)
 
         Spacer(Modifier.height(12.dp))
 
