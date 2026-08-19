ddfb47c98c4991013f3147a6ad2407d2b8014cce
jyvnee <jyunee54@gmail.com>
2026-08-20 01:39:12 +0900

    [feat/#182] 회원 탈퇴 UI 구현

---
+++ app/src/main/java/com/poti/android/presentation/user/withdrawal/WithdrawalScreen.kt
@@ -0,0 +1,89 @@
+package com.poti.android.presentation.user.withdrawal
+
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.poti.android.R
+import com.poti.android.core.designsystem.component.button.ActionButtonType
+import com.poti.android.core.designsystem.component.button.PotiActionButton
+import com.poti.android.core.designsystem.component.display.PotiListRadio
+import com.poti.android.core.designsystem.component.navigation.PotiHeaderPage
+import com.poti.android.core.designsystem.theme.PotiTheme
+import kotlinx.collections.immutable.toImmutableList
+
+@Composable
+fun WithdrawalRoute(modifier: Modifier = Modifier) {
+}
+
+@Composable
+fun WithdrawalScreen(
+    modifier: Modifier = Modifier,
+) {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        PotiHeaderPage(
+            onNavigationClick = {},
+            title = stringResource(R.string.withdrawal_title),
+        )
+
+        BoxWithConstraints(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(min = maxHeight)
+                    .verticalScroll(scrollState)
+                    .padding(horizontal = 16.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.withdrawal_reason_label),
+                    style = PotiTheme.typography.body14m,
+                    color = PotiTheme.colors.gray800,
+                    modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
+                )
+
+                PotiListRadio(
+                    options = stringArrayResource(R.array.withdrawal_reason_options).toImmutableList(),
+                    selectedOptionIndex = 1,
+                    onClick = {},
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                PotiActionButton(
+                    text = stringResource(R.string.withdrawal_button),
+                    onClick = {},
+                    type = ActionButtonType.DEACTIVE_MAIN,
+                    modifier = Modifier
+                        .padding(top = 4.dp, bottom = 14.dp)
+                        .fillMaxWidth(),
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun WithdrawalScreenPreview() {
+    WithdrawalScreen()
+}
