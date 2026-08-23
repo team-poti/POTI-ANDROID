595278909e5ba326b0c27f7c8bae43ecf31d4cd9
jyvnee <jyunee54@gmail.com>
2026-08-23 14:02:03 +0900

    [feat/#190] 내 프로필 관리 Contracts 작성

---
+++ app/src/main/java/com/poti/android/presentation/user/editprofile/model/Contracts.kt
@@ -0,0 +1,33 @@
+package com.poti.android.presentation.user.editprofile.model
+
+import android.net.Uri
+import com.poti.android.core.base.UiEffect
+import com.poti.android.core.base.UiIntent
+import com.poti.android.core.base.UiState
+import com.poti.android.core.common.state.ApiState
+
+data class EditProfileUiState(
+    val profileImageUrl: String? = null,
+    val selectedImageUri: Uri? = null,
+    val nickname: String = "",
+    val nicknameError: String? = null,
+    val isNicknameValid: Boolean = false,
+    val saveState: ApiState<Unit> = ApiState.Init,
+) : UiState {
+    val isSaveEnabled: Boolean
+        get() = isNicknameValid && nickname.isNotBlank()
+}
+
+sealed interface EditProfileUiIntent : UiIntent {
+    data object OnBackClick : EditProfileUiIntent
+
+    data class OnProfileImageSelected(val uri: Uri) : EditProfileUiIntent
+
+    data class OnNicknameChange(val value: String) : EditProfileUiIntent
+
+    data object OnSaveClick : EditProfileUiIntent
+}
+
+sealed interface EditProfileUiEffect : UiEffect {
+    data object NavigateBack : EditProfileUiEffect
+}
